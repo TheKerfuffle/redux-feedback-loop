27177bfbc6f23663c69953f55a41b3b396963349
Charlie Stokes <chstokes7@gmail.com>
2021-05-07 16:26:25 -0500

    redid store in index to support object oriented store

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -7,13 +7,26 @@ function Review() {
     const survey = useSelector(store => store.surveyStore);
     const dispatch = useDispatch();
 
+    function redoSurvey() {
+        history.push('/Home');
+        dispatch({type: 'RESET_SURVEY'});
+    }
+
+    function handleSubmit() {
+        history.push('/complete')
+    }
+
     return (
         <>
-            <h3>New Survey:</h3>
-            <h4>Feeling: {survey.feeling}</h4>
-            <h4>Understanding: {survey.understanding}</h4>
-            <h4>Supported: {survey.supported}</h4>
-            <h4>Comments: {survey.comments}</h4>
+            <form onSubmit={handleSubmit}>
+                <h3>New Survey:</h3>
+                <h4>Feeling: {survey.feeling}</h4>
+                <h4>Understanding: {survey.understanding}</h4>
+                <h4>Supported: {survey.supported}</h4>
+                <h4>Comments: {survey.comments}</h4>
+                <button type="submit">Submit</button>
+                <button onClick={redoSurvey}>Redo Survey</button>
+            </form>
         </>
 
     )
