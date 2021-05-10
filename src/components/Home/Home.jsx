2a0ab6620b9dcfd3206596a2ac6ba5f4e39a4e80
Charlie Stokes <chstokes7@gmail.com>
2021-05-09 19:16:56 -0500

    updated all pages with new material ui styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,17 @@
 import { useHistory } from 'react-router-dom';
+import { Grid, Paper, Typography, Button, makeStyles } from '@material-ui/core';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+
+const useStyles = makeStyles({
+    title: {
+        textDecoration: 'underline',
+        marginBottom: 20
+    },
+});
 
 function Home() {
+
+    const classes = useStyles();
 
     const history = useHistory();
 
@@ -11,8 +22,32 @@ function Home() {
 
     return (
         <>
-            <h2>Begin Survey:</h2>
-            <button onClick={beginSurvey}>Get Started!</button>
+            <Grid className={classes.feedback} container justify='center'>
+                <Grid item xs={10} >
+                    <Paper>
+                        <Grid item xs={12}>
+                            <Grid container justify='center'>
+                                <Typography
+                                    variant="h4"
+                                    color="primary"
+                                    className={classes.title}
+                                >
+                                    Begin Survey:
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Grid container justify='center'>
+                                <Button variant="contained"
+                                    onClick={beginSurvey}
+                                    endIcon={<KeyboardArrowRightIcon />}>
+                                    Begin Survey
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
         </>
 
     )
